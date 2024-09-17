04c6c42206b089741c4819a6ab60f0a2db94866a
joppe2001 <joppe.montezinos@gmail.com>
2024-09-17 09:56:28 +0200

    added fun visualization based on professors paper ;)

--- src/emoji_use.py
+++ src/emoji_use.py
@@ -1,19 +1,20 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data
-df = pd.read_csv('../data/whatsapp-20240910-221731.csv')
+def emoji_usage_chart(df, output_path):
+    emoji_usage = df[df['has_emoji']].groupby('author').size()
 
-# Count emoji usage by author
-emoji_usage = df[df['has_emoji']].groupby('author').size()
+    # Create a pie chart
+    plt.figure(figsize=(10, 6))
+    plt.pie(emoji_usage, labels=emoji_usage.index, autopct='%1.1f%%', startangle=90)
+    plt.title('Emoji Usage by Author')
+    plt.axis('equal')
 
-# Create a pie chart
-plt.figure(figsize=(10, 6))
-plt.pie(emoji_usage, labels=emoji_usage.index, autopct='%1.1f%%', startangle=90)
-plt.title('Emoji Usage by Author')
-plt.axis('equal')
-plt.show()
+    # Save the chart as an image file
+    plt.savefig(output_path)
+    plt.close()
 
-# Print the total number of messages with emojis
-print(f"Total messages with emojis: {emoji_usage.sum()}")
-print(f"Percentage of messages with emojis: {(emoji_usage.sum() / len(df) * 100):.2f}%")
+    # Print the total number of messages with emojis
+    total_emoji_messages = emoji_usage.sum()
+    percentage_emoji_messages = (total_emoji_messages / len(df) * 100)
+    print(f"Total messages with emojis: {total_emoji_messages}")
+    print(f"Percentage of messages with emojis: {percentage_emoji_messages:.2f}%")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from visualization import create_simple_message_frequency_plot
+from emoji_use import emoji_usage_chart
 import os
-
 
 def load_data(file_path):
     df = pd.read_csv(file_path)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
     return df
-
 
 def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,12 +16,13 @@ def main():
     df = load_data(data_path)
 
     fig = create_simple_message_frequency_plot(df)
-
     output_path = os.path.join(current_dir, '..', 'message_frequency.jpg')
-
     fig.write_image(output_path, scale=2)
-    print(f"Plot saved as {output_path}")
+    print(f"Message frequency plot saved as {output_path}")
 
+    output_path_emoji = os.path.join(current_dir, '..', 'emoji_usage.jpg')
+    emoji_usage_chart(df, output_path_emoji)
+    print(f"Emoji usage chart saved as {output_path_emoji}")
 
 if __name__ == "__main__":
     main()
