37d27a5e04bb087fe72c0bcf4a1c6c0d45dd03d9
joppe2001 <joppe.montezinos@gmail.com>
2024-09-17 15:56:33 +0200

    updated paths for saving images in main.py

--- src/main.py
+++ src/main.py
@@ -16,11 +16,11 @@ def main():
     df = load_data(data_path)
 
     fig = create_simple_message_frequency_plot(df)
-    output_path = os.path.join(current_dir, '..', 'message_frequency.jpg')
+    output_path = os.path.join(current_dir, '..', 'images','message_frequency.jpg')
     fig.write_image(output_path, scale=2)
     print(f"Message frequency plot saved as {output_path}")
 
-    output_path_emoji = os.path.join(current_dir, '..', 'emoji_usage.jpg')
+    output_path_emoji = os.path.join(current_dir, '..', 'images', 'emoji_usage.jpg')
     emoji_usage_chart(df, output_path_emoji)
     print(f"Emoji usage chart saved as {output_path_emoji}")
 
