51e39e2f27c08f457d8af2f803e97b8efcb18b73
vojatravel <development@voja.travel>
2024-10-08 12:57:03 +0200

    Add WhatsApp chat analysis and relationship plots
    Introduced `relations.py` to perform comprehensive analysis on WhatsApp chat data, including sentiment analysis and relationship plots. Updated `src/main.py` to integrate these new functionalities and modified the cleaning notebook to reflect the latest execution timestamps.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
+
+from relations import show_relations
 from visualization import create_simple_message_frequency_plot
 from emoji_use import emoji_usage_chart
 from timestamp import visualize_hourly_activity
 from distribution_vis import analyze_whatsapp_data
 from dist_test import sentiment_distribution_analysis
-from distribution import sentiment_analysis
+from relations import run_whatsapp_analysis
 import os
 
 def load_data(file_path):
@@ -35,8 +37,12 @@ def main():
     # sentiment_analysis(df, output_path_dist)
     # print("Analysis complete!")
 
-    output_path_dist_test = os.path.join(current_dir, '..', 'images')
-    sentiment_distribution_analysis(df, output_path_dist_test)
+    # output_path_dist_test = os.path.join(current_dir, '..', 'images')
+    # sentiment_distribution_analysis(df, output_path_dist_test)
+
+    run_whatsapp_analysis(df)
+
+    show_relations(df)
 
 if __name__ == "__main__":
     main()
