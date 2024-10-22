bca04b01377dcad9bb1e54a716522ec25f2e1332
vojatravel <development@voja.travel>
2024-10-22 16:00:20 +0200

    commit message nice

---
+++ src/base_classes.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class Regex:
+    phone_number: str = r'\b\d{3}[-.]?\d{3}[-.]?\d{4}\b'
+    date_message_split: str = r'(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s(.+)'
